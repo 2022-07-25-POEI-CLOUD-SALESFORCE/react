caa26f3196c53ccd0440eaa257eba331229e7321
Bruno-TAVERNIER <bruno@tavernier.eu>
2022-09-26 12:11:51 +0200

    test footer

--- src/Footer.test.js
+++ src/Footer.test.js
@@ -2,5 +2,14 @@ import { render, screen } from '@testing-library/react';
 import Footer from './Footer';
 
 // test qui vérifie que "copyright" est bien affiché
-
-//test qui vérifie que "2022" est bien affiché
+test('Footer Copyright', () => {
+  render(<Footer />);
+  const linkElement = screen.getByText(/copyright/i);
+  expect(linkElement).toBeInTheDocument();
+});
+// test qui vérifie que "2022" est bien affiché
+  test('Footer 2022', () => {
+    render(<Footer />);
+    const linkElement = screen.getByText(/2022/i);
+    expect(linkElement).toBeInTheDocument();
+  });
